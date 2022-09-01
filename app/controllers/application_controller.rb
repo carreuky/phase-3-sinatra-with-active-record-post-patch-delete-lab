e7ec754015fe3e98eb67aa93cab2fd237c4a5701
carreuky <carreuky@gmail.com>
2022-09-01 16:10:52 +0300

    commit tree 3109cc5725b26214e1365d3e7b6ac085ce4c69e7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,29 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name:params[:name],
+      price:params[:price],
+      bakery_id:params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  # delete
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
+
+  # patch
+  patch '/bakeries/:id' do
+    newB = Bakery.find(params[:id])
+    newB.update( name: params[:name])
+    newB.to_json
+  end
+
+
 end
